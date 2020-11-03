11801fc46a0a4f1de1686fc41b9f5a3d212d7ae1
fyfh <13757260730@sina.cn>
2020-11-04 00:08:06 +0800

    Update public.sh

--- public.sh
+++ public.sh
@@ -4,7 +4,7 @@
 
 # user_name="admin"                        # 用户名 admin
 # user_password=$user_name                 # 用户密码 admin
-# lan_ip="192.168.3"                       # lan 地址 192.168.3.1 一定别写后面的 .1
+lan_ip="192.168.12"                       # lan 地址 192.168.3.1 一定别写后面的 .1
 wifi_password="19960730"               # wifi密码，切记密码最少8位 admin
 version_time=$(date "+%Y%m%d")           # 自动时间更新时版本号: 20200320
 default_path="./user/shared/defaults.h"  # 默认文件配置目录
@@ -17,7 +17,7 @@ default_path="./user/shared/defaults.h"  # 默认文件配置目录
 # sed -i 's/#define\s*DEF_ROOT_PASSWORD\s*"admin"/#define  DEF_ROOT_PASSWORD     "'$user_password'"/g' $default_path
 
 # echo "修改Lan ip"
-# sed -i "s/192.168.2/$lan_ip/g" $default_path
+sed -i "s/192.168.2/$lan_ip/g" $default_path
 
 echo "修改Wif密码"
 sed -i "s/1234567890/$wifi_password/g" $default_path
